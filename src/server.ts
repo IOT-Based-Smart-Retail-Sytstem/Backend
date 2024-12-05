e3559c886d14fa2bb424e804f65ddfafecd38097
Haidyasser <haidyasser3@gmail.com>
2024-12-05 13:25:08 +0200

    add more logs

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import server from './app';
 import mongoose from 'mongoose';
 
+if (!process.env.PORT || !process.env.MONGO_URL) {
+  console.error('Missing required environment variables: PORT or MONGO_URL');
+  process.exit(1); // Exit with an error code
+}
+
 const port = process.env.PORT as string;
+const mongoUrl = process.env.MONGO_URL as string;
 
 mongoose
-  .connect(process.env.MONGO_URL as string)
+  .connect(mongoUrl)
   .then(() => server.listen(port))
-  .then(() => console.log(`connect to mongoDb and listen on port ${port}`))
-  .catch((err: any) => console.log(err));
+  .then(() => console.log(`Connected to MongoDB and listening on port ${port}`))
+  .catch((err: any) => console.log('Error connecting to MongoDB:', err));
+
