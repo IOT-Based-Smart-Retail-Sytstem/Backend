5e3f31d168c9407d385d24407c0b3758a6031d74
Haidyasser <haidyasser3@gmail.com>
2025-02-02 22:23:57 +0200

    edit DTOs structure

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { createContact, getContactById, getAllContacts } from '../service/contact.service';
 import log from '../utils/logger';
 
-export async function createContactHandler(req: Request, res: Response) {
+export async function createContactHandler(req: Request, res: Response, next: NextFunction) {
     const body = req.body;
     try {
         const contact = await createContact(body);
@@ -12,25 +12,33 @@ export async function createContactHandler(req: Request, res: Response) {
             data: contact
         })
     } catch (e: any) {
-        throw e;
+        next(e);
     }
 }
 
-export async function getContactHandler(req: Request, res: Response) {
+export async function getContactHandler(req: Request, res: Response, next: NextFunction) {
     const contactId = req.params.id;
     try {
         const contact = await getContactById(contactId);
-        res.send(contact);
+        res.status(200).json({
+            success: true,
+            message: "Contact fetched successfully",
+            data: contact
+        })
     } catch (e: any) {
-        throw e;
+        next(e);
     }
 }
 
-export async function getAllContactsHandler(req: Request, res: Response) {
+export async function getAllContactsHandler(req: Request, res: Response, next: NextFunction) {
     try {
         const contacts = await getAllContacts(req);
-        res.send(contacts);
+        res.status(200).json({
+            success: true,
+            message: "Contacts fetched successfully",
+            data: contacts
+        })
     } catch (e: any) {
-        throw e;
+        next(e);
     }
 }
