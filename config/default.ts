519dfa81d9222b53f0475e3e05f98b173723b5a3
Haidyasser <haidyasser3@gmail.com>
2025-06-14 18:44:17 +0300

    update dev script

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 export default{
-    port: 3000,
+    port: 4000,
     dbUri :  process.env.MONGO_URL, 
     logLevel : "info" ,
     accessTokenPrivateKey: "",
