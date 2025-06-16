a7a42fea904d0fe2a659b6742b9a93587c86a929
Haidyasser <haidyasser3@gmail.com>
2025-06-16 18:40:52 +0300

    change port number

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 export default{
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 3000,
     dbUri :  process.env.MONGO_URL, 
     logLevel : "info" ,
     accessTokenPrivateKey: "",
